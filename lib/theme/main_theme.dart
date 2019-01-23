e47afe859e0c6fb883f18dbf039ea0fb5b6e7aa7
Marcos Augusto <augustoprofemp@gmail.com>
2019-01-22 23:46:06 -0300

    [FEATURE] Adding font to application

--- lib/theme/main_theme.dart
+++ lib/theme/main_theme.dart
@@ -5,6 +5,7 @@ class MainTheme {
     return ThemeData(
         primaryColor: Color.fromRGBO(121, 102, 255, 1.0),
         accentColor: Color.fromRGBO(158, 183, 255, 1.0),
-        backgroundColor: Color.fromRGBO(241, 238, 252, 1.0));
+        backgroundColor: Color.fromRGBO(241, 238, 252, 1.0),
+        fontFamily: 'Nunito');
   }
 }
