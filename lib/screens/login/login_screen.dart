27fbb4474f763bf27f9d5c22c38c539ee020d13e
Marcos Augusto <augustoprofemp@gmail.com>
2019-01-23 23:16:53 -0300

    [FEATURE] Criando tela de login
    - Criando tela de login
    - Estilizando aplicação de modo geral (textos e botões)

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -7,18 +7,63 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        appBar: AppBar(title: Text('Teste'), backgroundColor: Colors.transparent, elevation: 0.0,),
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('just some kind of test')
+              Text(
+                "Let's get started",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0),
+              ),
+              Container(
+                child: Text(
+                  "Never a better time than now to start thinking about how you manage all your finances with ease",
+                  textAlign: TextAlign.center,
+                ),
+                padding: EdgeInsets.only(left: 32.0, right: 32.0, top: 20.0),
+              ),
+              Expanded(
+                child: Container(),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Expanded(
+                        child: Container(
+                      padding: EdgeInsets.only(left: 16.0, right: 16.0),
+                      child: RaisedButton(
+                        child: Text("Create account"),
+                        onPressed: () => {},
+                      ),
+                    )),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Expanded(
+                        child: Container(
+                      padding: EdgeInsets.only(left: 16.0, right: 16.0),
+                      child: RaisedButton(
+                        color: Colors.white,
+                        textColor: Colors.black,
+                        child: Text("Login"),
+                        onPressed: () => {},
+                      ),
+                    )),
+                  )
+                ],
+              ),
             ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
--- lib/theme/main_theme.dart
+++ lib/theme/main_theme.dart
@@ -5,7 +5,18 @@ class MainTheme {
     return ThemeData(
         primaryColor: Color.fromRGBO(121, 102, 255, 1.0),
         accentColor: Color.fromRGBO(158, 183, 255, 1.0),
-        backgroundColor: Color.fromRGBO(241, 238, 252, 1.0),
-        fontFamily: 'Nunito');
+        backgroundColor: Color.fromRGBO(255, 255, 255, 1.0),
+        scaffoldBackgroundColor: Color.fromRGBO(241, 238, 252, 1.0),
+        buttonTheme: ButtonThemeData(
+            textTheme: ButtonTextTheme.primary,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0))),
+        fontFamily: 'Nunito',
+        textTheme: TextTheme(
+            body1: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+            button: TextStyle(
+                fontSize: 16.0,
+                letterSpacing: 1.0,
+                fontWeight: FontWeight.bold)));
   }
 }
